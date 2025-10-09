ba42e7e568622de3082aab20db8fbdb6d4b038fe
zhivestracko <bormaka_@abv.bg>
2025-10-09 22:33:59 +0300

    ts intro

---
+++ index.ts
@@ -0,0 +1,8 @@
+console.log("Hello! My first ts code.")
+
+let fName: string = "Hello";// string text, let can be changed later on//
+const age: number = 20; //const can not be changed later on//
+let isTrue: boolean = 5 > 7; //true or false/
+
+let isFalse = (5<8 && 5> 10) === false;
+console.log(isFalse);
