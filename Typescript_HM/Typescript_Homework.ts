6432f00549da902b95a609f9d67bd7d07393b2ce
zhivestracko <bormaka_@abv.bg>
2025-10-26 13:04:06 +0200

    typescript-hm

--- Typescript_HM/Typescript_Homework.ts
+++ Typescript_HM/Typescript_Homework.ts
@@ -1,14 +1,14 @@
 let numberOne: number = 10;
 let numberTwo: number = 15;
-const Addition: number = numberOne + numberTwo;
-const Substraction: number = numberOne - numberTwo;
-console.log("The result of the addition is", Addition, "and the result of the substraction is", Substraction);
+const addition: number = numberOne + numberTwo;
+const substraction: number = numberOne - numberTwo;
+console.log("The result of the addition is", addition, "and the result of the substraction is", substraction);
 
-let Name: string = 'Ivan Petrov';
-let Age: number = 33;
+let name: string = 'Ivan Petrov';
+let age: number = 33;
 let city: string = 'Haskovo';
-const presentmyself: string = `Hi my name is ${Name}, I'm ${Age} years old and I'm from the town of ${city}`;
-console.log('Hi my name is', Name, 'and I am', Age ,'old', 'and I am from the city of', city); 
+const presentmyself: string = `Hi my name is ${name}, I'm ${age} years old and I'm from the town of ${city}`;
+console.log('Hi my name is', name, 'and I am', age ,'old', 'and I am from the city of', city); 
 console.log(presentmyself);
 
 let numOne: number = 35;
